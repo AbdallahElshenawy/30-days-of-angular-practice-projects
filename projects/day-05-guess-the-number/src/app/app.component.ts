73394f96165a71b99d2a1b3869577b0374d3598a
Abdallah <elshenawya894@gmail.com>
2026-02-04 10:54:20 +0200

    feat(day-05): guess the number game

--- projects/day-05-guess-the-number/src/app/app.component.ts
+++ projects/day-05-guess-the-number/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { GuessTheNumberComponent } from "./guess-the-number/guess-the-number.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ GuessTheNumberComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+<app-guess-the-number></app-guess-the-number>
   `,
   styles: [],
 })
