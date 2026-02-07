9ab3789d0c896c0e5dc18a3ce1d5d58fd16429c0
Abdallah <elshenawya894@gmail.com>
2026-02-07 14:02:11 +0200

    feat(day-08): Rock, Paper ,and Scissors

--- projects/day-08-rock-paper-scissors/src/app/app.component.ts
+++ projects/day-08-rock-paper-scissors/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { GameComponent } from "./game/game.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ GameComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+  <app-game></app-game>
   `,
   styles: [],
 })
